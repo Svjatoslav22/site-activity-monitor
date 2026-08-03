abde8de99b20dcc5a24988f22b8aae9cccf2cee5
Sviatoslav Tymtsiv <svtm1234567@gmail.com>
2026-08-03 15:37:44 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,11 @@ import { ConfigModule } from '@nestjs/config'; // 👈 Додали модуль
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MonitorsModule } from './monitors/monitors.module';
-// import { MonitorsModule } from './monitors/monitors.module';
 import { ChecksModule } from './checks/checks.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { UsersModule } from './users/users.module';
+import { TelegramModule } from './telegram/telegram.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
@@ -30,6 +30,7 @@ import { UsersModule } from './users/users.module';
     SchedulerModule, // 👈 Додали MonitorsModule
     ScheduleModule.forRoot(),
     UsersModule,
+    TelegramModule, // 👈 Додали TelegramModule для роботи з Telegram ботом
   ],
   controllers: [AppController],
   providers: [AppService],
